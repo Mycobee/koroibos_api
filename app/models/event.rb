2e4d6db7f55dc6e571be38af6e9857aeb2477f19
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-17 11:52:45 -0600

    Add event model

---
+++ app/models/event.rb
@@ -0,0 +1,7 @@
+class Event < ApplicationRecord
+  belongs_to :sport
+  has_many :olympian_events
+  has_many :olympians, through: :olympian_events
+  validates :name, presence: true
+  validates :games, presence: true
+end
