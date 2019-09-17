cf46927cc69fed5cd42ecec61bdb7fe3e57c35cd
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-17 17:24:39 -0600

    Add param filter logic for olympians index

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::OlympiansController < ApplicationController
 	def index
-    olympians = Olympian.all
-    render :json => olympians, each_serializer: OlympianSerializer
+    if !olympian_params[:age]
+      olympians = Olympian.all
+      render json: olympians, each_serializer: OlympianSerializer
+    else 
+      render json: Olympian.return_by_age(olympian_params[:age])
+    end
 	end
+
+  private
+
+  def olympian_params
+    params.permit(:age)
+  end
 end
 
