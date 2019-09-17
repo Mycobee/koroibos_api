eb78fe21a01276178daa3f31f6cb81d602367ff5
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-17 17:20:54 -0600

    Add spec for Olympian.age_filter class method

--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -1,6 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe Olympian, type: :model do
+  before :each do
+    ##olympian_data
+    olympian_1_attrs = {
+      name: "Bob Ross",
+      sex: "M",
+      age: 42,
+      height: 190,
+      weight: 140,
+      team: 'USA'
+    }
+    sport = Sport.create!(name: 'competitive painting')
+    @olympian_1 = sport.olympians.create!(olympian_1_attrs)
+    
+    olympian_2_attrs = {
+      name: "Prince",
+      sex: "M",
+      age: 22,
+      height: 160,
+      weight: 110,
+      team: 'USA'
+    }
+    sport_2 = Sport.create!(name: 'competitive dancing')
+    @olympian_2 = sport_2.olympians.create!(olympian_2_attrs)
+
+    olympian_3_attrs = {
+      name: "Garth Brooks",
+      sex: "F",
+      age: 12,
+      height: 120,
+      weight: 190,
+      team: 'Russia'
+    }
+    
+    sport_3 = Sport.create!(name: 'competitive yodling')
+    @olympian_3 = sport_3.olympians.create!(olympian_3_attrs)
+    ##Event data
+    event_1a_attrs = {
+      name: "bird draw",
+      games: "2020 couch party",
+    } 
+    
+    event_1b_attrs = {
+      name: "cloud draw",
+      games: "2020 couch party",
+    } 
+    
+    event_1c_attrs = {
+      name: "star draw",
+      games: "2020 couch party",
+    } 
+    
+    olympian_event_1a_attrs = {
+      medal: "NA"
+    }
+    olympian_event_1b_attrs = {
+      medal: "Gold"
+    }
+    olympian_event_1c_attrs = {
+      medal: "Bronze"
+    }
+    
+    
+    event_1a = sport.events.create!(event_1a_attrs)
+    event_1b = sport.events.create!(event_1b_attrs)
+    event_1c = sport.events.create!(event_1c_attrs)
+    
+    olympian_event_1a = @olympian_1.olympian_events.new(olympian_event_1a_attrs)
+    olympian_event_1a.event = event_1a
+    olympian_event_1a.save
+    
+    olympian_event_1b = @olympian_1.olympian_events.new(olympian_event_1b_attrs)
+    olympian_event_1b.event = event_1b
+    olympian_event_1b.save
+    
+    olympian_event_1c = @olympian_1.olympian_events.new(olympian_event_1c_attrs)
+    olympian_event_1c.event = event_1c
+    olympian_event_1c.save
+  end
+
+
   describe 'relationships' do
     it { should belong_to(:sport) }
     it { should have_many(:olympian_events) }
@@ -18,63 +98,22 @@ RSpec.describe Olympian, type: :model do
   end
 
   describe 'instance methods' do
-    before :each do
-      olympian_1_attrs = {
-        name: "Bob Ross",
-        sex: "M",
-        age: 22,
-        height: 190,
-        weight: 140,
-        team: 'USA'
-      }
-
-      event_1a_attrs = {
-        name: "bird draw",
-        games: "2020 couch party",
-      } 
-
-      event_1b_attrs = {
-        name: "cloud draw",
-        games: "2020 couch party",
-      } 
-
-      event_1c_attrs = {
-        name: "star draw",
-        games: "2020 couch party",
-      } 
-
-      olympian_event_1a_attrs = {
-        medal: "NA"
-      }
-      olympian_event_1b_attrs = {
-        medal: "Gold"
-      }
-      olympian_event_1c_attrs = {
-        medal: "Bronze"
-      }
-
-      sport = Sport.create!(name: 'competitive painting')
-      @olympian_1 = sport.olympians.create!(olympian_1_attrs)
-
-      event_1a = sport.events.create!(event_1a_attrs)
-      event_1b = sport.events.create!(event_1b_attrs)
-      event_1c = sport.events.create!(event_1c_attrs)
-
-      olympian_event_1a = @olympian_1.olympian_events.new(olympian_event_1a_attrs)
-      olympian_event_1a.event = event_1a
-      olympian_event_1a.save
-
-      olympian_event_1b = @olympian_1.olympian_events.new(olympian_event_1b_attrs)
-      olympian_event_1b.event = event_1b
-      olympian_event_1b.save
-
-      olympian_event_1c = @olympian_1.olympian_events.new(olympian_event_1c_attrs)
-      olympian_event_1c.event = event_1c
-      olympian_event_1c.save
-    end
-
     it '#medal_count' do
       expect(@olympian_1.medal_count).to eq(2)
+    end
+  end
+
+  describe 'class methods' do
+    it '.return_by_age' do
+      expect(Olympian.return_by_age('youngest')).to eq(@olympian_3)
+      expect(Olympian.return_by_age('oldest')).to eq(@olympian_1)
+    end
+
+    xit '.age_filter' do
+      valid_youngest_age_params = { "age": "youngest"}   
+
+      actual = Olympian.age_filter(valid_youngest_age_params)
+      expect(actual).to eq(@olympian_3)
     end
   end
 end
