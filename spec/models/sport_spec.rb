a5d37b1c38a0ae1f269080bd76e3371d640e7990
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-17 11:06:03 -0600

    Add relationship and validation spec to sports

--- spec/models/sport_spec.rb
+++ spec/models/sport_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Sport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:events) }
+    it { should have_many(:sports) }
+  end
+
+  describe 'validations' do
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_presence_of(:name) }
+  end
 end
