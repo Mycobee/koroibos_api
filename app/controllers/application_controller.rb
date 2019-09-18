23984bcb02d09632adc90d46adfb52603887aa23
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-17 22:23:43 -0600

    Add 404 not_found method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   def bad_request
     render json: { error: 'bad request' }, status: :bad_request
   end
+
+	def not_found
+    render json: { error: "not found" }, status: :not_found
+	end
 end
