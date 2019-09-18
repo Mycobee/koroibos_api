38c577d864bec37c24d999ca5d356b7ab872f247
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-17 18:10:25 -0600

    Add oldest age param filtering, and update conditional for index

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::OlympiansController < ApplicationController
 	def index
-    if olympian_params.keys.empty?
+    if params.keys == ["controller", "action"]
       olympians = Olympian.all
       render json: olympians, each_serializer: OlympianSerializer
     elsif olympian_params[:age] == 'youngest'
       render json: Olympian.youngest
+    elsif olympian_params[:age] == 'oldest'
+      render json: Olympian.oldest
     else 
       bad_request
     end
