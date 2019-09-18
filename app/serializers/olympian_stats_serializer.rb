b4a012e8157d302ada42385510bf35334bae2f5b
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-09-17 20:24:04 -0600

    Add completed serializer for olympian stats endpoint

--- app/serializers/olympian_stats_serializer.rb
+++ app/serializers/olympian_stats_serializer.rb
@@ -1,7 +1,19 @@
 class OlympianStatsSerializer < ActiveModel::Serializer
-  attributes :total_competing_olympians #, :average_weight, :average_age
+  attributes :total_competing_olympians, :average_weight, :average_age
 
   def total_competing_olympians
     self.object.total_competing_olympians
+  end
+
+  def average_weight
+    { 
+      "unit": "kg",
+      "male_olympians": self.object.average_weight('M'),
+      "female_olympians": self.object.average_weight('F')
+    }
+  end
+    
+  def average_age
+    self.object.average_age
   end
 end
